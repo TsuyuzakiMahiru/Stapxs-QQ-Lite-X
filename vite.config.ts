093e7d0fda2cdbaf24eed6073bc622ae231c01d3
露崎まひる <jingyee1900@live.cn>
2026-04-11 20:16:08 +0000

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -77,7 +77,7 @@ export function configFactory(outPath: string): UserConfigFnObject {
         return {
             root: './src/renderer',
             envDir: '../../',
-            base: env.VITE_CDN_BASE || './',
+            base: env.VITE_CDN_BASE || '/',
             server: {
                 port: 8080,
                 proxy: {
